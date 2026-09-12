4104d7b2aa45f9a2fb9b6e6cf06664f480ff702c
Venixa <liangkai851@gmail.com>
2026-09-13 07:36:49 +0800

    initial commit

---
+++ 21.py
@@ -0,0 +1,136 @@
+#this was made to practice defining statements
+#some very outdated code im not chaning cause lazy
+
+import random
+
+#------Lists and values------
+card_values = {
+    "A": 11,  # Can also be 1
+    "2": 2,
+    "3": 3,
+    "4": 4,
+    "5": 5,
+    "6": 6,
+    "7": 7,
+    "8": 8,
+    "9": 9,
+    "10": 10,
+    "J": 10,
+    "Q": 10,
+    "K": 10
+}
+
+suits = [
+"Hearts",
+"Spades",
+"Diamonds",
+"Clubs",
+]
+
+available_cards = []
+player_hand = []
+dealer_hand = []
+
+#------Deck System------
+for types in suits:
+    for ranks in card_values:
+        available_cards.append(ranks + " of " + types)
+random.shuffle(available_cards)
+
+#------Dealing System------
+def draw_card(hand):
+    hand.append(available_cards.pop(0))
+for i in range(2):
+    draw_card(player_hand)
+    draw_card(dealer_hand)
+
+#------Hand Calculator------
+def calculate_hand(hand):
+    hand_total = 0
+    ace_count = 0
+    for cards in hand:
+        hand_total += card_values[cards.split()[0]]
+        if cards.split()[0] == "A":
+            ace_count += 1
+    while ace_count >= 1 and hand_total > 21:
+        hand_total -= 10
+        ace_count -= 1
+    return hand_total
+
+def raw_total_calculator(hand):
+    ace_count = 0
+    raw_total = 0
+    for cards in hand:
+        raw_total += card_values[cards.split()[0]]
+        if cards.split()[0] == "A":
+            ace_count += 1
+    processed_total = calculate_hand(hand)
+    return ace_count,raw_total,processed_total
+
+
+def ace_dropped(hand):
+    _, raw_total, processed_total = raw_total_calculator(hand)
+    current_ace = (raw_total - processed_total) / 10
+    if current_ace >= 1:
+        print("since you have reaced >21 and have a ace, your ace turns into 1")
+
+#------soft17 logic------
+def soft_17(hand):
+    ace_count, raw_total, processed_total = raw_total_calculator(hand)
+    current_ace = (raw_total - processed_total)  / 10
+    return ace_count - current_ace >= 1 and processed_total == 17
+
+#------Main------
+player_bust = False
+dealer_bust = False
+while True:
+    calculate_player_hand = calculate_hand(player_hand)
+    print("your cards:", ', '.join(player_hand))
+    print("your total:", calculate_player_hand)
+    print (f'dealers first card: {dealer_hand[0]}')
+    command = input("commands, hit, stand : ").strip().lower()
+
+    if command == "hit":
+        draw_card(player_hand)
+        ace_dropped(player_hand)
+        current_player_hand_total = calculate_hand(player_hand)
+        if current_player_hand_total > 21:
+            player_bust = True
+            print("Bust!")
+            print("your cards at the end:", ', '.join(player_hand))
+            print("your total:", current_player_hand_total)
+            break
+    elif command == "stand":
+        print("player decided to stand")
+        break
+    else:
+        print("Unknown command. Don't just make things up.")
+
+#------Dealer Logic------
+while player_bust is not True:
+    calculate_dealer_hand = calculate_hand(dealer_hand)
+    print("Dealer's hand: ", ', '.join(dealer_hand))
+    print("Dealer's total:", calculate_dealer_hand)
+
+    if calculate_dealer_hand > 21:
+        dealer_bust = True
+        break
+    if (calculate_dealer_hand == 17 and not soft_17(dealer_hand)) or calculate_dealer_hand >= 18:
+        print("dealer decided to stand")
+        break
+    else:
+        print("dealer decided to hit")
+        draw_card(dealer_hand)
+
+#------Stand-off------
+if player_bust:
+    print("dealer Win")
+elif dealer_bust:
+    print("Player Win")
+else:
+    if calculate_player_hand == calculate_dealer_hand:
+        print("tie, no one wins")
+    elif calculate_player_hand > calculate_dealer_hand:
+        print("Player Win")
+    elif calculate_player_hand < calculate_dealer_hand:
+        print("Dealer Win")
---
+++ 21V2.py
@@ -0,0 +1,266 @@
+#this is my first public project, nothing crazy just blackjack
+#i kind of wanted to learn about classes so i wrote this
+#constructive criticism is appreciated
+import random
+
+card_values = {
+    "A": 11,  # Can also be 1
+    "2": 2,
+    "3": 3,
+    "4": 4,
+    "5": 5,
+    "6": 6,
+    "7": 7,
+    "8": 8,
+    "9": 9,
+    "10": 10,
+    "J": 10,
+    "Q": 10,
+    "K": 10
+}
+
+suits = [
+"Hearts",
+"Spades",
+"Diamonds",
+"Clubs",
+]
+
+
+class Deck:
+    def __init__(self, card_values, suits):
+        self.card_values = card_values
+        self.suits = suits
+        self.deck_constructer()
+
+    def deck_constructer(self):
+        available_cards = []
+        for types in self.suits:
+            for ranks in self.card_values:
+                available_cards.append(ranks + " of " + types)
+        random.shuffle(available_cards)
+        self.available_cards = available_cards
+
+    def draw(self):
+        return self.available_cards.pop(0)
+
+
+class Hand:
+    def __init__(self, hand, deck, card_values):
+        self.hand = hand
+        self.deck = deck
+        self.card_values = card_values
+
+    def calculate_hand(self):
+        total = sum(self.card_values[card.split()[0]] for card in self.hand)
+        aces = sum(card.split()[0] == "A" for card in self.hand)
+        adjusted_total = total
+        aces_as_one = 0
+
+        while aces_as_one < aces and adjusted_total > 21:
+            adjusted_total -= 10
+            aces_as_one += 1
+        self.raw_total = total
+        self.calculated_hand = adjusted_total
+        self.ace_count = aces
+        return adjusted_total
+
+    def draw_card(self):
+        self.hand.append(self.deck.draw())
+        self.calculate_hand()
+
+    def ace_dropped(self):
+        current_ace = (self.raw_total - self.calculated_hand)  / 10
+        if current_ace >= 1:
+            print("since you have reaced >21 and have a ace, your ace turns into 1")
+
+    def soft_17(self):
+        current_ace = (self.raw_total - self.calculated_hand)  / 10
+        return self.ace_count - current_ace >= 1 and self.calculated_hand == 17
+
+
+class Player:
+    def __init__(self, balance=1000):
+        self.balance = balance
+
+    def place_bet(self,amount):
+        if amount > self.balance:
+            return False
+        else:
+            self.balance -= amount
+            self.placed_bet = amount
+            return True
+
+    def win_bet(self):
+        self.balance += self.placed_bet*2
+        self.placed_bet = 0
+
+    def tie_bet(self):
+        self.balance += self.placed_bet
+        self.placed_bet = 0
+
+    def lose_bet(self):
+        self.placed_bet = 0
+
+    def money(self,amount):
+        self.balance += amount
+
+def show_balance():
+    print(f"\nYour Balance: ${player.balance}")
+
+
+def message_banner(message):
+    print(
+    f"{'-' * 30}\n"
+    f"{message}\n"
+    f"{'-' * 30}")
+
+
+#-----main-----
+player = Player()
+xray = False
+win_button = False
+menu_commands = ["play", "balance", "exit", "cheats"]
+while True:
+    command = input(f"Commands: {", ".join(menu_commands)}: ").strip().lower()
+    if command in ["play", "1"]:
+        bet_placed = False
+        #-----Betting System-----
+        while True:
+            show_balance()
+            if player.balance > 0:
+                try:
+                    bet = int(input("How much to bet?: "))
+                    if bet < 0:
+                        raise ValueError
+                    if player.place_bet(bet):
+                        print(f"\nYou placed a bet of ${bet}")
+                        bet_placed = True
+                        break
+                    else:
+                        print(f"You don't have enough money missing amount: ${bet - player.balance}") 
+                        continue
+                except ValueError:
+                    print("Invalid Input, needs to be a positive whole number.")
+                    continue
+            else:
+                break
+
+        if bet_placed:
+            deck = Deck(card_values, suits)
+            player_hand = Hand([], deck, card_values)
+            dealer_hand = Hand([], deck, card_values)
+            for _ in range(2):
+                player_hand.draw_card()
+                dealer_hand.draw_card()
+            #-----Playerblock-----
+            player_bust = False
+            dealer_bust = False
+            game_commands = ["Hit", "Stand"]
+            while True:
+                calculate_player_hand = player_hand.calculate_hand()
+                message_banner(
+                f"Your cards: {', '.join(player_hand.hand)}\n"
+                f"Your total: {calculate_player_hand}\n"
+                f"Dealer's first card: {dealer_hand.hand[0]}"
+                )
+                game_command = input(f"Commands: {", ".join(game_commands)}: ").strip().lower()
+                if game_command == "hit":
+                    player_hand.draw_card()
+                    player_hand.ace_dropped()
+                    if player_hand.calculate_hand() > 21:
+                        player_bust = True
+                        print(
+                        "\nBust!\n"
+                        f"your cards at the end: {', '.join(player_hand.hand)}\n"
+                        f"your total: {player_hand.calculate_hand()}"
+                        )
+                        break
+                elif game_command == "stand":
+                    print("player decided to stand")
+                    break
+                elif game_command == "deck" and xray:
+                    print(f"next 5 upcomming cards: {', '.join(deck.available_cards[:5])}")
+                elif game_command == "dealer" and xray:
+                    print(f"{', '.join(dealer_hand.hand)}")
+                elif game_command == "win" and win_button:
+                    dealer_bust = True
+                    break
+                else:
+                    print("Unknown command. Don't just make things up.")
+
+            #------Dealer Logic------
+            while player_bust == False and dealer_bust == False:
+                calculate_dealer_hand = dealer_hand.calculate_hand()
+                print(
+                    f"\nDealer's hand: {', '.join(dealer_hand.hand)}\n"
+                    f"Dealer's total: {calculate_dealer_hand}"
+                    )
+                if calculate_dealer_hand > 21:
+                    dealer_bust = True
+                    print("Dealer Has Busted")
+                    break
+                if (calculate_dealer_hand == 17 and not dealer_hand.soft_17()) or calculate_dealer_hand >= 18:
+                    print("dealer decided to stand")
+                    break
+                print("dealer decided to hit")
+                dealer_hand.draw_card()
+
+            #------Stand-off------
+            if player_bust:
+                message_banner("Player Lost")
+                player.lose_bet()
+                show_balance()
+            elif dealer_bust:
+                message_banner("Player Won!")
+                player.win_bet()
+                show_balance()
+            else:
+                if calculate_player_hand == calculate_dealer_hand:
+                    message_banner("Player Tied")
+                    player.tie_bet()
+                    show_balance()
+                elif calculate_player_hand > calculate_dealer_hand:
+                    message_banner("Player Won!")
+                    player.win_bet()
+                    show_balance()
+                elif calculate_player_hand < calculate_dealer_hand:
+                    message_banner("Player Lost")
+                    player.lose_bet()
+                    show_balance()
+        else:
+            print("No money to bet")
+#Others
+    #check balance
+    elif command in ["balance", "2"]:
+        show_balance()
+    #exit
+    elif command in ["exit", "3"]:
+        print("Exiting, Bye!")
+        exit()
+
+#Cheats
+    elif command in ["cheats", "4"]:
+        print(
+        "\nCheat commands:\n"
+        "Money, spawns in money\n"
+        "Xray, see dealer/deck cards\n"
+        "Winbutton, win button."
+        )
+    #spawn in money cause why not
+    elif command == "money":
+        try:
+            player.money(int(input("How much money do you want: ")))
+            show_balance()
+        except ValueError:
+            print("\nInvalid Input, needs to be a positive whole number.")
+    #see cards
+    elif command == "xray":
+        xray = True
+        print("\nxray enabled, during hit/stand type in deck/dealer to see the cards")
+    #auto win
+    elif command == "winbutton":
+        win_button = True
+        print("\nwin button enabled, during hit/stand type win to instantly win")
+    else:
+        print("\nunknown command")
